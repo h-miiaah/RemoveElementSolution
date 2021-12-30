71c933849f97a40fe1973951e4923c1d1a1da648
h-miiaah <haim.miiaah5@gmail.com>
2021-12-29 23:24:14 -0500

    conditional completed

--- RemoveElementSolution.js
+++ RemoveElementSolution.js
@@ -6,6 +6,8 @@
  var removeElement = function(nums, val) {
     let count = 0;
     for (let i = 0; i < nums.length; i++){
-        
+        if(nums[i] !== val){
+            nums[count++] = nums[i];
+        }
     }
 };
